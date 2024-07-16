e0c9d359620e8fe36c1287effbc11eb2c32b816e
Nordine Bittich <contact@bittich.be>
2024-07-16 10:44:34 +0200

    forgot the external route

--- app/router.js
+++ app/router.js
@@ -1,6 +1,6 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'frontend-local-harvester/config/environment';
-import { fallbackRoute } from 'ember-metis';
+import { externalRoute, fallbackRoute } from 'ember-metis';
 
 export default class Router extends EmberRouter {
   location = config.locationType;
@@ -10,6 +10,7 @@ export default class Router extends EmberRouter {
 Router.map(function () {
   this.route('sparql');
   this.route('view', function () {});
+  externalRoute(this); // <-- Add this line before fallbackRoute
   fallbackRoute(this);
 
   this.route('legaal', function () {
